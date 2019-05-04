added GWA correction, fixed reporting and buffer calculation

config.py
@@ -9,7 +9,7 @@ import os
 param = {}
 param["region"] = 'Europe'
 param["year"] = '2015'
-param["technology"] = ['PV']  #['PV', 'CSP', 'WindOn', 'WindOff']
+param["technology"] = ['WindOn']  #['PV', 'CSP', 'WindOn', 'WindOff']
 param["quantiles"] = np.array([100, 97, 95, 90, 75, 67, 50, 30])
 param["savetiff"] = 1  # Save geotiff files of mask and weight rasters
 param["nproc"] = 6
@@ -35,8 +35,9 @@ param["res_desired"] = np.array([1/240, 1/240])
 # 6   -- Closed shrublands
 # 7   -- Open shrublands
 # 8   -- Woody savannas
-# 9   -- Grasslands
-# 10  -- Permanent wetland
+# 9   -- Savannas
+# 10  -- Grasslands
+# 11  -- Permanent wetland
 # 12  -- Croplands
 # 13  -- URBAN AND BUILT-UP
 # 14  -- Croplands / natural vegetation mosaic
@@ -111,7 +112,7 @@ csp["weight"] = {"lu_availability": np.array([0.00, 0.00, 0.00, 0.00, 0.00, 0.00
 windon = {}
 windon["resource"] = {"res_correction": 1,
                       "topo_correction": 1,
-                      "topo_factors": (0.0004, -0.1)
+                      "topo_weight": 'capacity' # 'none' or 'size' or 'capacity'
                       }
 windon["technical"] = {"w_in": 4,
                        "w_r": 15,
@@ -167,8 +168,9 @@ year = param["year"]
 paths = {}
 
 # Shapefiles
-PathTemp = root + "INPUTS" + fs + region + fs + "Shapefile" + fs + region
-paths["SHP"] = PathTemp + "_NUTS0_wo_Balkans_with_EEZ.shp"
+PathTemp = root + "INPUTS" + fs + region + fs + "Shapefile" + fs
+paths["SHP"] = PathTemp + "Bayern_in_Europe_with_EEZ.shp"
+paths["Countries"] = PathTemp + region + "_NUTS0_wo_Balkans_with_EEZ.shp" # for eventual correction with the Global Wind Atlas
 
 # MERRA2
 PathTemp = root + "INPUTS" + fs + region + fs + "MERRA2 " + year + fs
@@ -179,7 +181,10 @@ paths["W50M"] = PathTemp + "w50m_" + year + ".mat"
 paths["GHI"] = PathTemp + "swgdn_" + year + ".mat"
 paths["TOA"] = PathTemp + "swtdn_" + year + ".mat"
 paths["CLEARNESS"] = PathTemp + "clearness_" + year + ".mat"
-paths["T2M"] = PathTemp + "t2m_" + year + ".mat"  # Temperature
+paths["T2M"] = PathTemp + "t2m_" + year + ".mat"
+
+# IRENA
+paths["inst-cap"] = root + "INPUTS" + fs + region + fs + "IRENA " + year + fs + "inst_cap_" + year + ".csv"
 
 # Global maps
 PathTemp = root + "INPUTS" + fs + "Global maps" + fs
@@ -188,6 +193,7 @@ paths["Topo_tiles"] = PathTemp + "Topography" + fs
 paths["Pop_tiles"] = PathTemp + "Population" + fs
 paths["Bathym_global"] = PathTemp + "Bathymetry" + fs + "ETOPO1_Ice_c_geotiff.tif"
 paths["Protected"] = PathTemp + "Protected Areas" + fs + "WDPA_Nov2018-shapefile-polygons.shp"
+paths["GWA"] = PathTemp + "Global Wind Atlas" + fs + fs + "windSpeed.csv"
 
 # Local maps
 PathTemp = root + "INPUTS" + fs + region + fs + region
@@ -200,11 +206,12 @@ paths["SLOPE"] = PathTemp + "_Slope.tif"  # Slope
 paths["BATH"] = PathTemp + "_Bathymetry.tif"  # Bathymetry
 paths["POP"] = PathTemp + "_Population.tif"  # Population
 paths["BUFFER"] = PathTemp + "_Population_Buffered.tif"  # Buffered population
+paths["CORR_GWA"] = PathTemp + "_GWA_Correction.mat"  # Correction factors based on the GWA
 paths["CORR"] = PathTemp + "_Wind_Correction.tif"  # Correction factors for wind speeds
 
 # Ouput Folders
 timestamp = str(datetime.datetime.now().strftime("%Y%m%dT%H%M%S"))
-timestamp = "test"
+timestamp = "20190502 Referenzszenario"
 paths["OUT"] = root + "OUTPUTS" + fs + region + fs + timestamp + fs
 if not os.path.isdir(paths["OUT"]):
     os.mkdir(paths["OUT"])

data_functions.py
@@ -4,6 +4,9 @@ import math as m
 import numpy as np
 import rasterio
 from rasterio import windows, mask, MemoryFile
+import pandas as pd
+import hdf5storage
+from itertools import product
 
 
 def calc_ext(regb, ext, res):
@@ -116,11 +119,83 @@ def calc_region(region, Crd_reg, res_desired, GeoRef):
         A_region = out_image[0]
 
     return A_region
+	
+	
+def calc_gwa_correction(param, paths):
+    ''' description'''
+    m_high = param["m_high"]
+    n_high = param["n_high"]
+    res_desired = param["res_desired"]
+    nCountries = param["nCountries"]
+    countries_shp = param["countries"]
+    Crd_countries = param["Crd_countries"][0:nCountries, :]
+    GeoRef = param["GeoRef"]
+
+    # Obtain wind speed at 50m
+    W50M = hdf5storage.read('W50M', paths["W50M"])
+    W50M = np.mean(W50M, 2)
+    W50M = resizem(W50M, m_high, n_high)
+	
+    # Obtain topography
+    with rasterio.open(paths["TOPO"]) as src:
+        w = src.read(1)
+    TOPO = np.flipud(w)
+	
+    # Get the installed capacities
+    inst_cap = pd.read_csv(paths["inst-cap"], skiprows = 2, sep = ';', index_col = 0)
+
+    w_size = np.zeros((nCountries, 1))
+    w_cap = np.zeros((nCountries, 1))
+    # Try different combinations of (a, b)
+    combi_list = list(product(np.arange(0.00046, 0.00066, 0.00002), np.arange(-0.3, 0, 0.025)))
+    errors = np.zeros((len(combi_list), nCountries))
+    for reg in range(0, nCountries):
+        A_region = calc_region(countries_shp.iloc[reg], Crd_countries[reg, :], res_desired, GeoRef)
+        reg_name = countries_shp.iloc[reg]["NAME_SHORT"]
+        Ind_reg = np.nonzero(A_region)
+        w_size[reg] = len(Ind_reg[0])
+        w_cap[reg] = inst_cap.loc[reg_name, 'WindOn']
+
+        # Load MERRA data, increase its resolution, and fit it to the extent
+        w50m_reg = W50M[Ind_reg]
+        topo_reg = TOPO[Ind_reg]
+		
+        # Get the sampled frequencies from the GWA
+        w50m_gwa = pd.read_csv(paths["GWA"][:-14] + reg_name + paths["GWA"][-14:], usecols=['gwa_ws']).to_numpy()[:,0]
+        
+        i = 0
+        for combi in combi_list:
+            ai, bi = combi
+            w50m_corrected = w50m_reg * np.minimum(np.exp(ai * topo_reg + bi), 3.5)
+            w50m_sorted = np.sort(w50m_corrected)
+            w50m_sampled = np.flipud(w50m_sorted[::(len(w50m_sorted) // 50 + 1)])
+            w50m_diff = w50m_sampled - w50m_gwa
+            errors[i, reg] = np.sqrt((w50m_diff**2).sum())
+            i = i + 1
+			
+    w_size = np.tile(w_size / w_size.sum(), (1, len(combi_list))).transpose()
+    w_cap = np.tile(w_cap / w_cap.sum(), (1, len(combi_list))).transpose()
+	
+    ae, be = combi_list[np.argmin(np.sum(errors / nCountries, 1))]
+    correction_none = np.zeros(TOPO.shape)
+    correction_none = np.minimum(np.exp(ae * TOPO + be), 3.5)
+	
+    a_size, b_size = combi_list[np.argmin(np.sum(errors * w_size, 1))]
+    correction_size = np.zeros(TOPO.shape)
+    correction_size = np.minimum(np.exp(a_size * TOPO + b_size), 3.5)
+	
+    a_cap, b_cap = combi_list[np.argmin(np.sum(errors * w_cap, 1))]
+    correction_capacity = np.zeros(TOPO.shape)
+    correction_capacity = np.minimum(np.exp(a_cap * TOPO + b_cap), 3.5)
+
+    hdf5storage.writes({'correction_none': correction_none, 'correction_size': correction_size, 'correction_capacity': correction_capacity,}, paths["CORR_GWA"],
+                       store_python_metadata=True, matlab_compatible=True)
+    return
 
 
 def calc_gcr(Crd_all, m_high, n_high, res_desired, GCR):
     """
-    This function creates a GCR weighting matrix for the desired geographic extent
+    This function creates a GCR weighting matrix for the desired geographic extent.
     The sizing of the PV system is conducted on a user-defined day for a shade-free exposure
     to the sun during a given number of hours.
 
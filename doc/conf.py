9e030cdfb2a5a7a5319b9103d9375b0a0da08979
HoussameH <houmyh@gmail.com>
2019-09-03 09:38:21 +0200

    Documentation - clean and themed

--- doc/conf.py
+++ doc/conf.py
@@ -13,6 +13,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sphinx_rtd_theme
 
 
 # -- Project information -----------------------------------------------------
@@ -24,14 +25,14 @@ author = 'Kais Siala, Houssame Houmy'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
-
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
-]
+extensions = ['sphinx.ext.autodoc',
+              "sphinx_rtd_theme",
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['ntemplates']
@@ -41,13 +42,12 @@ templates_path = ['ntemplates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

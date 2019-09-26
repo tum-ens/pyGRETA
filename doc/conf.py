31c98002aa38095908de6f86985bc4149062f0f4
kais-siala <kais.siala@tum.de>
2019-09-26 10:54:39 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -74,7 +74,7 @@ html_theme_options = {
 latex_documents = {
     'startdocname': '',  # (path) Start file for the documentation to be included in PDF, can be left empty to use default index.rst
     'targetname': project,  # (str) Output name of the Latex file generated
-    'title':,  # (str) Title of the Latex/pdf file, can be left empty to use the title of startdocname
+    'title': '',  # (str) Title of the Latex/pdf file, can be left empty to use the title of startdocname
     'author': 'Kais Siala, \\Sergio Alejandro Huezo Rodriguez, \\and Houssame Houmy. ',  # (str) Authors, use \\ to separate authors (e.i. 'John \\and Sarah')
     'documentclass': '',  # not clear
     'toctree_only': True  # (bool) Include startdocname in the latex/pdf ? can be used to have different first pages. The first toctree entry in startdocname will be used.

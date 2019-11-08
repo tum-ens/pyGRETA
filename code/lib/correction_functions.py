2e582b5df6ab74ec2f864b09e3819ae82df4de4b
HoussameH <houmyh@gmail.com>
2019-11-08 16:41:59 +0100

    Changed output files format from GWA

--- code/lib/correction_functions.py
+++ code/lib/correction_functions.py
@@ -210,7 +210,7 @@ def calc_gwa_correction(paths, param):
         topo_reg = TOPO[Ind_reg]
 
         # Get the sampled frequencies from the GWA
-        w50m_gwa = pd.read_csv(paths["GWA"][:-14] + reg_name + paths["GWA"][-14:], usecols=["gwa_ws"]).to_numpy()[:, 0]
+        w50m_gwa = pd.read_csv(paths["GWA"][:-14] + reg_name + paths["GWA"][-14:], usecols=[0]).to_numpy()[:, 0]
 
         i = 0
         for combi in combi_list:

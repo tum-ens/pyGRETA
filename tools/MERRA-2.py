feab217e1bb007a00b94ef0475f87da6fcf5a348
Patrick Buchenberg <patrick.buchenberg@tum.de>
2021-09-30 11:40:43 +0200

    Added MERRA-2 download feature

---
+++ tools/MERRA-2.py
@@ -0,0 +1,40 @@
+# How to create authentication file for downloading data with GES DISC account
+# On Mac/Linux:
+# 1) cd ~ or cd $HOME
+# 2) touch .netrc
+# 3) echo "machine urs.earthdata.nasa.gov login <uid> password <password>" >> .netrc (where <uid> is your user name and <password> is your Earthdata Login password without the brackets)
+# 4) chmod 0600 .netrc (so only you can access it)
+# On Windows:
+# 1) Open Notepad
+# 2) Enter (without quotes): machine urs.earthdata.nasa.gov login <uid> password <password>
+# 3) Save as: C:\Users\<username>\.netrc
+
+import os
+import requests
+
+# Parameters
+file_links = "subset_M2I1NXLFO_5.12.4_20210930_073038.txt"  # Downloaded links from GES DISC
+path_downloadedFiles = "download/"
+
+# Create folder for downloaded files
+if not os.path.exists(path_downloadedFiles):
+    os.makedirs(path_downloadedFiles)
+
+# Download each link of text file
+file = open(file_links, 'r')
+Lines = file.readlines()
+
+for line in Lines:
+    url = line.strip()
+    filename = url.split("/")[-1]
+
+    if not os.path.isfile(path_downloadedFiles + filename):
+        result = requests.get(url)
+        try:
+            result.raise_for_status()
+            f = open(path_downloadedFiles + filename, 'wb')
+            f.write(result.content)
+            f.close()
+            print('contents of URL written to ' + filename)
+        except:
+            print('requests.get() returned an error code ' + str(result.status_code))
